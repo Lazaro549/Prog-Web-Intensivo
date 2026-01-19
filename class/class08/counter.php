110bf054f0d5744cb014497de258bc7265a7e105
Lazaro Gomez Vitolo <lazarogvitolo@gmail.com>
2026-01-19 01:07:32 -0300

    Update counter.php

--- class/class08/counter.php
+++ class/class08/counter.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contador</title>
+    <title>Counter</title>
 </head>
 <body>
 <?php
-if(isset($_GET["valor"])){
-    $valor=$_GET["valor"];
-    $proximo=$valor+1;
-    $anterior=$valor-1;
+if(isset($_GET["value"])){
+    $value=$_GET["value"];
+    $next=$value+1;
+    $before=$valor-1;
 }else{
-    $valor=0;
-    $proximo=1;
-    $anterior=-1;
+    $value=0;
+    $next=1;
+    $before=-1;
 }
 ?>
-<h1 align="center"><?php echo $valor; ?></h1>
+<h1 align="center"><?php echo $value; ?></h1>
 <br>
 <p align="center">
-    <a href="contador.php?valor=<?php echo $proximo; ?>">Sumar</a> &nbsp;
-    <a href="contador.php?valor=<?php echo $anterior; ?>">Restar</a>
+    <a href="counter.php?value=<?php echo $next; ?>">add</a> &nbsp;
+    <a href="counter.php?value=<?php echo $before; ?>">sub</a>
 <br>
-    <a href="contador.php">Reset</a>
+    <a href="counter.php">Reset</a>
 </p>
 
 </body>
+
 </html>
